05e8c6d62ab0962c7ebc6e0037d3eca2b68d4b31
Yan Zhou <zhouyan@me.com>
2019-10-02 01:25:23 +0800

    arrow support

--- python/bson_dataframe/arrow.py
+++ python/bson_dataframe/arrow.py
@@ -21,100 +21,207 @@ from .visitor import *
 import pyarrow
 
 
-def _make_mask(array: pyarrow.Array):
-    if array.null_count == 0:
-        return None
+class _SchemaToArrow(Visitor):
+    def visit_bool(self, schema):
+        return pyarrow.bool_()
 
-    mask = array.buffers()[0]
-    vals = numpy.ndarray(len(mask), numpy.uint8, mask)
-    bits = numpy.unpackbits(vals, bitorder='little')
-    data = bits[array.offset:(array.offset + len(array))]
+    def visit_numeric(self, schema):
+        return getattr(pyarrow, schema.name)()
 
-    return numpy.packbits(data)
+    def visit_date(self, schema):
+        return pyarrow.date32()
+
+    def visit_timestamp(self, schema):
+        return pyarrow.timestamp(schema.unit)
+
+    def visit_time(self, schema):
+        return getattr(pyarrow, f'time{schema.byte_width * 8}')(schema.unit)
+
+    def visit_opaque(self, schema):
+        return pyarrow.binary(schema.byte_width)
+
+    def visit_bytes(self, schema):
+        return pyarrow.binary()
+
+    def visit_utf8(self, schema):
+        return pyarrow.utf8()
+
+    def visit_dictionary(self, schema):
+        index = schema.index.accept(self)
+        value = schema.value.accept(self)
+        ordered = isinstance(schema, Ordered)
+        return pyarrow.dictionary(index, value, ordered)
+
+    def visit_list(self, schema):
+        value = schema.value.accept(self)
+        return pyarrow.list_(value)
+
+    def visit_struct(self, schema):
+        fields = [pyarrow.field(k, v.accept(self)) for k, v in schema.fields]
+        return pyarrow.struct(fields)
 
 
-def _make_counts(buf: pyarrow.Buffer, array: pyarrow.Array):
-    counts = buf.slice(array.offset * 4, len(array) * 4 + 4)
-    counts = numpy.frombuffer(counts, numpy.int32)
-    counts = numpy.diff(counts, prepend=counts[0])
-
-    return counts
+def _schema_to_arrow(self):
+    return self.accept(_SchemaToArrow())
 
 
-class _ToArrow(Visitor):
+def _schema_from_arrow(dtype):
+    assert isinstance(dtype, pyarrow.DataType)
+
+    if pyarrow.types.is_boolean(dtype):
+        return Bool()
+
+    if pyarrow.types.is_int8(dtype):
+        return Int8()
+
+    if pyarrow.types.is_int16(dtype):
+        return Int16()
+
+    if pyarrow.types.is_int32(dtype):
+        return Int32()
+
+    if pyarrow.types.is_int64(dtype):
+        return Int64()
+
+    if pyarrow.types.is_uint8(dtype):
+        return UInt8()
+
+    if pyarrow.types.is_uint16(dtype):
+        return UInt16()
+
+    if pyarrow.types.is_uint32(dtype):
+        return UInt32()
+
+    if pyarrow.types.is_uint64(dtype):
+        return UInt64()
+
+    if pyarrow.types.is_float16(dtype):
+        return Float16()
+
+    if pyarrow.types.is_float32(dtype):
+        return Float32()
+
+    if pyarrow.types.is_float64(dtype):
+        return Float64()
+
+    if pyarrow.types.is_date32(dtype):
+        return Date()
+
+    if pyarrow.types.is_date64(dtype):
+        return Timestamp('ms')
+
+    if pyarrow.types.is_timestamp(dtype):
+        assert dtype.tz is None
+        return Timestamp(dtype.unit)
+
+    if dtype.equals(pyarrow.time32('s')):
+        return Time('s')
+
+    if dtype.equals(pyarrow.time32('ms')):
+        return Time('ms')
+
+    if dtype.equals(pyarrow.time64('us')):
+        return Time('us')
+
+    if dtype.equals(pyarrow.time64('ns')):
+        return Time('ns')
+
+    if pyarrow.types.is_binary(dtype):
+        return Bytes()
+
+    if pyarrow.types.is_string(dtype):
+        return Utf8()
+
+    if pyarrow.types.is_fixed_size_binary(dtype):
+        return Opaque(dtype.byte_width)
+
+    if pyarrow.types.is_dictionary(dtype):
+        stype = Ordered if dtype.ordered else Factor
+        index = _schema_from_arrow(dtype.index_type)
+        value = _schema_from_arrow(dtype.value_type)
+        return stype(index, value)
+
+    if pyarrow.types.is_list(dtype):
+        value = _schema_from_arrow(dtype.value_type)
+        return List(value)
+
+    if pyarrow.types.is_struct(dtype):
+        fields = ((v.name, _schema_from_arrow(v.type)) for v in dtype)
+        return Struct(fields)
+
+    raise ValueError(f'{dtype} is not supported BSON DataFrame type')
+
+
+class _ArrayToArrow(Visitor):
+    def _make_buffer(self, data):
+        if data is None:
+            return data
+
+        if isinstance(data, pyarrow.Buffer):
+            return data
+
+        if isinstance(data, bytes):
+            return pyarrow.py_buffer(data)
+
+        if isinstance(data, numpy.ndarray):
+            return pyarrow.py_buffer(data.tobytes())
+
+        with memoryview(data) as view:
+            return pyarrow.py_buffer(view.tobytes())
+
     def _make_mask(self, array):
-        mask = array.numpy_mask(False)
-
-        if mask is numpy.ma.nomask:
-            return None
-
-        mask = numpy.packbits(mask, bitorder='little')
-        return pyarrow.py_buffer(mask.tobytes())
+        mask = numpy.frombuffer(array.mask, numpy.uint8)
+        mask = numpy.unpackbits(mask)
+        null = len(array) - numpy.sum(mask)
+        mask = None if null == 0 else numpy.packbits(mask, bitorder='little')
+        return null, mask
 
     def _make_offsets(self, array):
         counts = numpy.frombuffer(array.counts, numpy.int32)
-        offsets = numpy.cumsum(counts, dtype=numpy.int32)
-        return pyarrow.py_buffer(offsets.tobytes())
+        return numpy.cumsum(counts, dtype=numpy.int32)
 
-    def visit_null(self, array):
-        dtype = pyarrow.null()
-        buffers = [None]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+    def _make_array(self, array, data=None, *, offsets=None, children=None):
+        dtype = _schema_to_arrow(array.schema)
+        null_count, mask = self._make_mask(array)
+
+        buffers = [self._make_buffer(mask)]
+
+        if offsets is not None:
+            buffers.append(self._make_buffer(offsets))
+
+        if data is not None:
+            buffers.append(self._make_buffer(data))
+
+        return pyarrow.Array.from_buffers(dtype,
+                                          len(array),
+                                          buffers,
+                                          null_count=null_count,
+                                          offset=0,
+                                          children=children)
 
     def visit_bool(self, array):
-        dtype = pyarrow.bool_()
         data = numpy.frombuffer(array.data, bool)
-        bits = numpy.packbits(data, bitorder='little')
-        buffers = [self._make_mask(array), pyarrow.py_buffer(bits)]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        data = numpy.packbits(data, bitorder='little')
+        return self._make_array(array, data)
 
     def visit_numeric(self, array):
-        dtype = getattr(pyarrow, array.schema.name)()
-        buffers = [self._make_mask(array), pyarrow.py_buffer(array.data)]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        return self._make_array(array, array.data)
 
     def visit_date(self, array):
-
-        if array.schema.unit == 'd':
-            dtype = pyarrow.date32()
-
-        if array.schema.unit == 'ms':
-            dtype = pyarrow.date64()
-
-        buffers = [self._make_mask(array), pyarrow.py_buffer(array.data)]
-
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        return self._make_array(array, array.data)
 
     def visit_timestamp(self, array):
-        dtype = pyarrow.timestamp(array.schema.unit)
-        buffers = [self._make_mask(array), pyarrow.py_buffer(array.data)]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        return self._make_array(array, array.data)
 
     def visit_time(self, array):
-        dtype = getattr(pyarrow, f'time{array.schema.byte_width * 8}')
-        dtype = dtype(array.schema.unit)
-        buffers = [self._make_mask(array), pyarrow.py_buffer(array.data)]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        return self._make_array(array, array.data)
 
     def visit_opaque(self, array):
-        dtype = pyarrow.binary(array.schema.byte_width)
-        buffers = [self._make_mask(array), pyarrow.py_buffer(array.data)]
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        return self._make_array(array, array.data)
 
     def visit_binary(self, array):
-        if isinstance(array.schema, Bytes):
-            dtype = pyarrow.binary()
-
-        if isinstance(array.schema, Utf8):
-            dtype = pyarrow.utf8()
-
-        buffers = [
-            self._make_mask(array),
-            self._make_offsets(array),
-            pyarrow.py_buffer(array.data)
-        ]
-
-        return pyarrow.Array.from_buffers(dtype, len(array), buffers)
+        offsets = self._make_offsets(array)
+        return self._make_array(array, array.data, offsets=offsets)
 
     def visit_dictionary(self, array):
         index = array.index.accept(self)
@@ -125,186 +232,124 @@ class _ToArrow(Visitor):
                                                    ordered=ordered)
 
     def visit_list(self, array):
-        values = array.value.accept(self)
-        dtype = pyarrow.list_(values.type)
-        buffers = [self._make_mask(array), self._make_offsets(array)]
-        children = [values]
-        return pyarrow.Array.from_buffers(dtype,
-                                          len(array),
-                                          buffers,
-                                          children=children)
+        offsets = self._make_offsets(array)
+        children = [array.value.accept(self)]
+        return self._make_array(array, offsets=offsets, children=children)
 
     def visit_struct(self, array):
-        fields = [(k, v.accept(self)) for k, v in array.fields]
-        dtype = pyarrow.struct([pyarrow.field(k, v.type) for k, v in fields])
-        buffers = [self._make_mask(array)]
-        children = [v for _, v in fields]
-        return pyarrow.Array.from_buffers(dtype,
-                                          len(array),
-                                          buffers,
-                                          children=children)
+        children = [v.accept(self) for _, v in array.fields]
+        return self._make_array(array, children=children)
 
 
-def _FromArrow(Visitor):
+def _array_to_arrow(self):
+    return self.accept(_ArrayToArrow())
+
+
+class _ArrayFromArrow(Visitor):
     def __init__(self, array):
+        assert isinstance(array, pyarrow.Array)
+
         self.array = array
 
-    def _make_data(self):
-        pass
-
     def _make_mask(self):
-        pass
+        if self.array.null_count == 0:
+            return None
 
-    def visit_null(self, schema):
-        return NullArray(len(self.array))
+        mask = self.array.buffers()[0]
+        mask = numpy.frombuffer(mask, numpy.uint8)
+        mask = numpy.unpackbits(mask, bitorder='little')
+        mask = mask[self.array.offset:(self.array.offset + len(self.array))]
+
+        return numpy.packbits(mask)
+
+    def _make_counts(self):
+        counts = self.array.buffers()[1]
+        counts = counts.slice(self.array.offset * 4, len(self.array) * 4 + 4)
+        counts = numpy.frombuffer(counts, numpy.int32)
+        start = counts[0]
+        length = counts[-1] - start
+        counts = numpy.diff(counts, prepend=start)
+
+        return counts, start, length
 
     def visit_bool(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask())
+        atype = array_type(schema)
+        data = self.array.buffers()[-1]
+        data = numpy.frombuffer(data, numpy.uint8)
+        data = numpy.unpackbits(data, bitorder='little')
+        data = data[self.array.offset:(self.array.offset + len(self.array))]
+        mask = self._make_mask()
+        return atype(data, mask)
+
+    def _make_data(self, schema, start=None, length=None):
+        if start is None:
+            assert schema.byte_width * 8 == self.array.type.bit_width
+            start = self.array.offset * schema.byte_width
+
+        if length is None:
+            assert schema.byte_width * 8 == self.array.type.bit_width
+            length = len(self.array) * schema.byte_width
+
+        data = self.array.buffers()[-1].slice(start, length)
+        mask = self._make_mask()
+
+        return data, mask
 
     def visit_numeric(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask())
+        atype = array_type(schema)
+        return atype(*self._make_data(schema))
 
     def visit_date(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask(),
-                                  schema=schema)
+        atype = array_type(schema)
+        return atype(*self._make_data(schema))
 
     def visit_timestamp(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask(),
-                                  schema=schema)
+        atype = array_type(schema)
+        return atype(*self._make_data(schema), schema=schema)
 
     def visit_time(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask(),
-                                  schema=schema)
+        atype = array_type(schema)
+        return atype(*self._make_data(schema), schema=schema)
 
     def visit_opaque(self, schema):
-        return array_type(schema)(self._make_data(schema.byte_width),
-                                  self._make_mask(),
-                                  schema=schema)
+        atype = array_type(schema)
+        return atype(*self._make_data(schema), schema=schema)
 
     def visit_binary(self, schema):
+        atype = array_type(schema)
         counts, start, length = self._make_counts()
-        return array_type(schema)(self._make_data(start, length),
-                                  self._make_mask(),
-                                  counts=counts)
+        data, mask = self._make_data(schema, start, length)
+        return atype(data, mask, length=len(self.array), counts=counts)
 
-    def vist_dictionary(self, schema):
-        index = schema.index.accept(self.array.indices)
-        value = schema.value.accept(self.array.dictionary)
+    def visit_dictionary(self, schema):
+        index = schema.index.accept(_ArrayFromArrow(self.array.indices))
+        value = schema.value.accept(_ArrayFromArrow(self.array.dictionary))
         return array_type(schema)(index, value)
 
     def visit_list(self, schema):
-        counts, start, end = self._make_counts()
-        values = self.array.flatten().slice(start, length)
-        return ListArray(schema.value.accept(values), counts=counts)
+        counts, start, length = self._make_counts()
+        data = self.array.flatten().slice(start, length)
+        data = schema.value.accept(_ArrayFromArrow(data))
+        mask = self._make_mask()
+        return ListArray(data, mask, length=len(self.array), counts=counts)
 
     def visit_struct(self, schema):
         fields = list()
         for i, kv in enumerate(schema.fields):
             k, v = kv
-            fields.append(v.accept(self.array.field[i]))
+            data = self.array.field(i)
+            data = v.accept(_ArrayFromArrow(data))
+            fields.append((k, data))
         return StructArray(fields)
 
 
-def arrow_schema(data):
-    if pyarrow.types.is_null(data.type):
-        return Null()
-
-    if pyarrow.types.is_boolean(data.type):
-        return Bool()
-
-    if pyarrow.types.is_int8(data.type):
-        return Int8()
-
-    if pyarrow.types.is_int16(data.type):
-        return Int16()
-
-    if pyarrow.types.is_int32(data.type):
-        return Int32()
-
-    if pyarrow.types.is_int64(data.type):
-        return Int64()
-
-    if pyarrow.types.is_uint8(data.type):
-        return UInt8()
-
-    if pyarrow.types.is_uint16(data.type):
-        return UInt16()
-
-    if pyarrow.types.is_uint32(data.type):
-        return UInt32()
-
-    if pyarrow.types.is_uint64(data.type):
-        return UInt64()
-
-    if pyarrow.types.is_float16(data.type):
-        return Float16()
-
-    if pyarrow.types.is_float32(data.type):
-        return Float32()
-
-    if pyarrow.types.is_float64(data.type):
-        return Float64()
-
-    if pyarrow.types.is_date32(data.type):
-        return Date('d')
-
-    if pyarrow.types.is_date64(data.type):
-        return Date('ms')
-
-    if pyarrow.types.is_timestamp(data.type):
-        assert data.type.tz is None
-        return Timestamp(data.type.unit)
-
-    if data.type.equals(pyarrow.time32('s')):
-        return Time('s')
-
-    if data.type.equals(pyarrow.time32('ms')):
-        return Time('ms')
-
-    if data.type.equals(pyarrow.time64('us')):
-        return Time('us')
-
-    if data.type.equals(pyarrow.time64('ns')):
-        return Time('ns')
-
-    if pyarrow.types.is_binary(data.type):
-        return Bytes()
-
-    if pyarrow.types.is_string(data.type):
-        return Utf8()
-
-    if pyarrow.types.is_fixed_size_binary(data.type):
-        return Opaque(data.type.byte_width)
-
-    if pyarrow.types.is_dictionary(data.type):
-        index = arrow_schema(data.indices)
-        value = arrow_schema(data.dictionary)
-        if data.type.ordered:
-            return Ordered(index, value)
-        else:
-            return Factor(index, value)
-
-    if pyarrow.types.is_list(data.type):
-        return List(arrow_schema(data.flatten()))
-
-    if pyarrow.types.is_struct(data.type):
-        return Struct([(v.name, arrow_schema(data.field(i)))
-                       for i, v in enumerate(data.type)])
+def _array_from_arrow(array):
+    schema = _schema_from_arrow(array.type)
+    return schema.accept(_ArrayFromArrow(array))
 
 
-def to_arrow(array):
-    return array.accept(_ToArrow())
+Schema.to_arrow = _schema_to_arrow
+Schema.from_arrow = staticmethod(_schema_from_arrow)
 
-
-def from_arrow(data):
-    schema = arrow_schema(data)
-
-    if schema is None:
-        raise ValueError(f'Unable to deduce schema from dtype {data.dtype}')
-
-    return schema.accept(_FromArrow(data))
+Array.to_arrow = _array_to_arrow
+Array.from_arrow = staticmethod(_array_from_arrow)
